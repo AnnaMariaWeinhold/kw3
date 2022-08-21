83dbb03afad8406456e12fdd5dd4c46829fc1bee
Magpie <anna.maria.weinhold@posteo.de>
2022-08-21 10:33:55 +0200

    stand von 21.08.2022

---
+++ de/scripts/index.js
@@ -0,0 +1,15 @@
+<script type='text/javascript'>;
+
+
+
+    $(document).ready(function(){
+
+        //Beim Klicken auf den Menu-Button:
+
+        $('a#mobile-menu-btn').click(function () {
+          //...soll die Navigation eingeblendet werden.
+        $('#navigation').slideToggle("slow");
+    });
+
+});
+</script>
